a0a8c8b324a90ec15085b3c855246a2fd108d1be
Canvas-Toolkit <canvas.toolkit@rediffmail.com>
2023-10-08 13:28:11 +0530

    Components removed setup avoid

--- projects/canvaskit/src/lib/canvaskit.module.ts
+++ projects/canvaskit/src/lib/canvaskit.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CanvaskitComponent } from './canvaskit.component';
 
 
 
 @NgModule({
   declarations: [
-    CanvaskitComponent
   ],
   imports: [
   ],
   exports: [
-    CanvaskitComponent
   ]
 })
 export class CanvaskitModule { }
--- projects/canvaskit/src/public-api.ts
+++ projects/canvaskit/src/public-api.ts
@@ -3,5 +3,4 @@
  */
 
 export * from './lib/canvaskit.service';
-export * from './lib/canvaskit.component';
 export * from './lib/canvaskit.module';
