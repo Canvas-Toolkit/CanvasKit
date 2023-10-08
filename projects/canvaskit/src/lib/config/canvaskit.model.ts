b6ca3a0569108fd7de0942f56c5b1249cc666bf3
Canvas-Toolkit <canvas.toolkit@rediffmail.com>
2023-10-08 14:46:02 +0530

    Added interface and basic implementation for setup working start

--- projects/canvaskit/src/lib/config/canvaskit.model.ts
+++ projects/canvaskit/src/lib/config/canvaskit.model.ts
@@ -1,4 +1,4 @@
-interface CanvasKit {
+export interface CanvasKit {
     drawRect(x: number, y: number, width: number, height: number): void;
     drawCircle(x: number, y: number, radius: number): void;
     drawLine(x1: number, y1: number, x2: number, y2: number): void;
---
+++ projects/canvaskit/src/lib/config/webgl/webgl.ts
@@ -0,0 +1,40 @@
+import { CanvasKit } from "../canvaskit.model";
+
+export class WebGL implements CanvasKit {
+
+    drawRect(x: number, y: number, width: number, height: number): void {
+      throw new Error('Not implemented');
+    }
+  
+    drawCircle(x: number, y: number, radius: number): void {
+      throw new Error('Not implemented');
+    }
+  
+    drawLine(x1: number, y1: number, x2: number, y2: number): void {
+      throw new Error('Not implemented');
+    }
+  
+    drawText(text: string, x: number, y: number): void {
+      throw new Error('Not implemented');
+    }
+  
+    drawImage(image: HTMLImageElement, x: number, y: number): void {
+      throw new Error('Not implemented');
+    }
+  
+    applyFilter(filter: string, element: HTMLCanvasElement): void {
+      throw new Error('Not implemented');
+    }
+  
+    applyEffect(effect: string, element: HTMLCanvasElement): void {
+      throw new Error('Not implemented');
+    }
+  
+    saveAsFile(filename: string, format: 'png' | 'jpeg' | 'webp'): void {
+      throw new Error('Not implemented');
+    }
+  
+    getImageData(format: 'png' | 'jpeg' | 'webp'): Promise<Uint8Array> {
+      throw new Error('Not implemented');
+    }
+  }
