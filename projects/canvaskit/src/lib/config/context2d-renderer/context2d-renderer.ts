8a4a247902506c649dd8d06f40fe3b6f43bd59aa
Saloni Sharma <92209331+salonisharma-96@users.noreply.github.com>
2023-10-14 21:31:38 +0530

    implementation provides a basic idea

--- projects/canvaskit/src/lib/config/context2d-renderer/context2d-renderer.ts
+++ projects/canvaskit/src/lib/config/context2d-renderer/context2d-renderer.ts
@@ -1,40 +1,69 @@
 import { CanvasKit, ImageFormat } from "../canvaskit.model";
 
-export class Context2dRenderer implements CanvasKit {
+enum ImageFormat {
+  PNG,
+  JPEG,
+  GIF,
+  // Add more formats as needed
+}
 
-    drawRect(x: number, y: number, width: number, height: number): void {
-      throw new Error('Not implemented');
-    }
-  
-    drawCircle(x: number, y: number, radius: number): void {
-      throw new Error('Not implemented');
-    }
-  
-    drawLine(x1: number, y1: number, x2: number, y2: number): void {
-      throw new Error('Not implemented');
-    }
-  
-    drawText(text: string, x: number, y: number): void {
-      throw new Error('Not implemented');
-    }
-  
-    drawImage(image: HTMLImageElement, x: number, y: number): void {
-      throw new Error('Not implemented');
-    }
-  
-    applyFilter(filter: string, element: HTMLCanvasElement): void {
-      throw new Error('Not implemented');
-    }
-  
-    applyEffect(effect: string, element: HTMLCanvasElement): void {
-      throw new Error('Not implemented');
-    }
-  
-    saveAsFile(filename: string, format: ImageFormat): void {
-      throw new Error('Not implemented');
-    }
-  
-    getImageData(format: ImageFormat): Promise<Uint8Array> {
-      throw new Error('Not implemented');
-    }
+class Context2dRenderer implements CanvasKit {
+  private canvas: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
   }
+
+  drawRect(x: number, y: number, width: number, height: number): void {
+    this.ctx.fillRect(x, y, width, height);
+  }
+
+  drawCircle(x: number, y: number, radius: number): void {
+    this.ctx.beginPath();
+    this.ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    this.ctx.fill();
+  }
+
+  drawLine(x1: number, y1: number, x2: number, y2: number): void {
+    this.ctx.beginPath();
+    this.ctx.moveTo(x1, y1);
+    this.ctx.lineTo(x2, y2);
+    this.ctx.stroke();
+  }
+
+  drawText(text: string, x: number, y: number): void {
+    this.ctx.fillText(text, x, y);
+  }
+
+  drawImage(image: HTMLImageElement, x: number, y: number): void {
+    this.ctx.drawImage(image, x, y);
+  }
+
+  applyFilter(filter: string, element: HTMLCanvasElement): void {
+    // Apply the specified filter to the element's canvas
+    // This might involve manipulating the canvas pixel data.
+    // You would need to implement this based on the filter.
+  }
+
+  applyEffect(effect: string, element: HTMLCanvasElement): void {
+    // Apply the specified effect to the element's canvas
+    // This could involve various graphical transformations.
+    // Implement this based on the desired effect.
+  }
+
+  saveAsFile(filename: string, format: ImageFormat): void {
+    // Save the current canvas as a file with the given filename and format.
+    // You might need to use techniques such as creating a data URL.
+  }
+
+  getImageData(format: ImageFormat): Promise<Uint8Array> {
+    // Get the pixel data of the canvas in the specified format
+    // This would involve reading pixel data from the canvas and encoding it.
+    // Return a Promise with the pixel data.
+    return new Promise<Uint8Array>((resolve, reject) => {
+      // Implement the logic to retrieve and convert pixel data here.
+    });
+  }
+}
